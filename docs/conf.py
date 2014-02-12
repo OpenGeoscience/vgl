09f6e98e7e32ce4630213fb228e530d11131e8a5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-02-12 15:09:09 -0500

    Using bootstrap theme now

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 
 import sys, os
 
+# At the top.
+import sphinx_bootstrap_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -91,7 +94,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+
+# Activate the theme.
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
