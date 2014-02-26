38107cac577c66cce5885ab8ec614910c20be4bc
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-02-25 19:07:35 -0500

    Removed bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -103,12 +103,12 @@ pygments_style = 'sphinx'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
+#html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "VGL",
+    #'navbar_title': "VGL",
 
     # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': "Site",
+    #'navbar_site_name': "Site",
 
     # A list of tuples containing pages or urls to link to.
     # Valid tuples should be in the following forms:
@@ -117,19 +117,19 @@ html_theme_options = {
     #    (name, "http://example.com", True) # arbitrary absolute url
     # Note the "1" or "True" value above as the third argument to indicate
     # an arbitrary url.
-    'navbar_links': [
-        ("GitHub", "https://github.com/OpenGeoscience/vgl", True),
-    ],
+    #'navbar_links': [
+    #    ("GitHub", "https://github.com/OpenGeoscience/vgl", True),
+    #],
 
     # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': True,
+    #'navbar_sidebarrel': True,
 
     # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': True,
+    #'navbar_pagenav': True,
 
     # Global TOC depth for "site" navbar tab. (Default: 1)
     # Switching to -1 shows all levels.
-    'globaltoc_depth': 2,
+    #'globaltoc_depth': 2,
 
     # Include hidden TOCs in Site navbar?
     #
@@ -138,19 +138,19 @@ html_theme_options = {
     # will break.
     #
     # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
+    #'globaltoc_includehidden': "true",
 
     # HTML navbar class (Default: "navbar") to attach to <div> element.
     # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar navbar-inverse",
+    #'navbar_class': "navbar navbar-inverse",
 
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
+    #'navbar_fixed_top': "true",
 
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "",
+    #'source_link_position': "",
 
     # Bootswatch (http://bootswatch.com/) theme.
     #
@@ -161,7 +161,7 @@ html_theme_options = {
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
     #'bootstrap_version': "3",
-}
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
